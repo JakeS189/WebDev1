f37d409895e08f1929b5fefc27a03c179ea5b175
JakeS189 <228929@shakopeeschools.org>
2022-05-18 09:28:51 -0500

    Jake S
    Lots of other stuff(Mainly images from
    1.7)

---
+++ Version7.0/user16/JS/SampleJS.js
@@ -0,0 +1,49 @@
+<body>
+
+
+
+
+
+<script>
+function changeColor(){
+  <h6> var newColor = document.getElementById('colorPicker').value;
+	document.getElementById('colorMsg').style.background = newColor;
+
+
+</h6>
+}
+
+</script>
+<div id="colorMsg" style="font-size:18px;width:150px;height:100px;padding:5px;">Choose a background color...</div> 
+<select id="colorPicker" onchange="JavaScript:changeColor()">
+<option value="transparent">None</option>
+<option value="yellow">Yellow</option>
+<option value="salmon">Salmon</option>
+<option value="lightblue">Light Blue</option>
+<option value="limegreen">Lime Green</option>
+</select>
+
+<script>
+  function Function3() {
+    alert("Thank you for visitng my website") 
+    
+    }
+  
+  </script>
+  
+  
+  <body onload="Function3()"></body>
+
+
+  <script>
+            
+            <function> Function2() {
+              alert("Thank You!")
+            }
+          </function>
+          </script>
+            <button onclick="Function2()" type="submit" class="btn btn-outline-light mb-4" style="background-color: rgb(115, 113, 113)">
+              Subscribe
+            </button>
+
+</body>
